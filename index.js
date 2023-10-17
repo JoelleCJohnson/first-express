3f5dfe531766805533092c9e64499e0a1747e1dd
Joelle  Johnson <joellej221@gmail.com>
2023-10-17 11:58:01 -0400

    added line 5 index.js to allow us to post from postman

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { addStudent, getAllStudents } from "./src/students.js";
+
 const app = express(); //app is now the guard
+app.use(express.json())//allows us to use json file on postman
 
 //list of allowed requests
 app.get("/students", getAllStudents) //functions don't have ()
