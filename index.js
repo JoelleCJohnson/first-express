cb38316f62c1735d42aa8d48044d8409a71e2298
Joelle  Johnson <joellej221@gmail.com>
2023-10-17 11:55:14 -0400

    removed hello function

--- index.js
+++ index.js
@@ -6,11 +6,6 @@ const app = express(); //app is now the guard
 app.get("/students", getAllStudents) //functions don't have ()
 app.post("/student", addStudent)
 
-
-app.get("/hello", (request, response) => {
-    response.send("Hello there!")
-})
-
 //Tells api to go "guard the door"(aka port)
 app.listen(3000, () => {
     console.log('Listening on http://localhost:3000...')
